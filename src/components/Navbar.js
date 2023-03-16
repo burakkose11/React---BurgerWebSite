a92e21e13a892bf284f7ca1769fdceb5cc95266d
Burak Kose <burakkose011@icloud.com>
2023-03-17 01:24:32 +0300

    navbar olusturuldu, css ozellikleri eklendi

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import BurgerLogo from "../assets/burgerlogo.png";
+import '../styles/Navbar.css'
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="main">
+        <img src={BurgerLogo} />
+        <div className="mainLink" >
+          <Link to="/">Anasayfa</Link>
+          <Link to="/menu">Menü</Link>
+          <Link to="/about">Hakkımızda</Link>
+          <Link to="/contact">İletişim</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
