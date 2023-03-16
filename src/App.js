eb49ddea0072434b0e06bd8c14eafec7f1bd27d8
Burak Kose <burakkose011@icloud.com>
2023-03-17 01:05:55 +0300

    routing olusturuldu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./components/Home";
+import { Menu } from "./components/Menu";
+import { About } from "./components/About";
+import { Contact } from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/menu" exact element={<Menu />} />
+        <Route path="/about" exact element={<About />} />
+        <Route path="/contact" exact element={<Contact />} />
+      </Routes>
     </div>
   );
 }
