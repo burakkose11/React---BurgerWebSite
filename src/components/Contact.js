95718a7051e336bb4869005a530b36f631db2fa8
Burak Kose <burakkose011@icloud.com>
2023-03-17 14:53:32 +0300

    iletisim kismi eklendi

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from "react";
+import BannerImage from "../assets/banner.png";
+import "../styles/Contact.css";
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="contact">
+      <div
+        className="leftSide"
+        style={{ backgroundImage: `url(${BannerImage})` }}
+      ></div>
+      <div className="rightSide">
+        <h1>Bizimle İletişime Geçin</h1>
+        <form>
+          <label>Ad Soyad</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Lütfen Adınızı Soyadınızı Giriniz.."
+          />
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Lütfen Emailinizi Giriniz.."
+          />
+          <label>Mesajınız</label>
+          <textarea
+            rows="6"
+            name="message"
+            placeholder="Lütfen Mesajınızı Giriniz"
+          ></textarea>
+          <button>Gönder</button>
+        </form>
+      </div>
+    </div>
+  );
+};
