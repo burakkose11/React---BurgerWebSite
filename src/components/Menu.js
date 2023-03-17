56e5a41a282fa074d19e89cf24ab837359078953
Burak Kose <burakkose011@icloud.com>
2023-03-17 14:10:57 +0300

    menu eklendi

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Data } from "../helpers/Data";
+import MenuItem from "../components/MenuItem";
+import "../styles/Menu.css";
 
-export const Menu = () => {
+function Menu() {
   return (
-    <div>Menu</div>
-  )
+    <div className="menu">
+      <h1 className="menuTitle">Burgerlerimiz</h1>
+      <div className="menuList">
+        {Data.map((menuItem, index) => {
+          return (
+            <MenuItem
+              key={index}
+              image={menuItem.image}
+              name={menuItem.name}
+              content={menuItem.content}
+              price={menuItem.price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
+
+export default Menu;
