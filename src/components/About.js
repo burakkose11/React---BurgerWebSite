8a9928c30423a3838e2ff2184b93ae96af716eaa
Burak Kose <burakkose011@icloud.com>
2023-03-17 14:36:22 +0300

    about eklendi

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from "react";
+import BannerImage from "../assets/banneryeni.jpg";
+import "../styles/About.css";
 
 export const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="about">
+      <div
+        className="aboutTop"
+        style={{ backgroundImage: `url(${BannerImage})` }}
+      ></div>
+      <div className="aboutBottom">
+        <h1>Hakkımızda</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero magnam
+          iste, quae illum numquam vero blanditiis dolorem enim labore
+          aperiam.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Libero magnam iste, quae illum numquam vero blanditiis dolorem enim
+          labore aperiam.Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Libero magnam iste, quae illum numquam vero blanditiis dolorem
+          enim labore aperiam.Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Libero magnam iste, quae illum numquam vero blanditiis dolorem
+          enim labore aperiam.Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Libero magnam iste, quae illum numquam vero blanditiis dolorem
+          enim labore aperiam.Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Libero magnam iste, quae illum numquam vero blanditiis dolorem
+          enim labore aperiam.Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Libero magnam iste, quae illum numquam vero blanditiis dolorem
+          enim labore aperiam.Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Libero magnam iste, quae illum numquam vero blanditiis dolorem
+          enim labore aperiam.
+        </p>
+      </div>
+    </div>
+  );
+};
